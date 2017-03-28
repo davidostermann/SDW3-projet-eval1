6a14d1e0340c404f23185008a07807d7ab8d5358
Drakncel <florian.pigis@gmail.com>
2017-03-28 19:23:15 +0200

    Change color working

--- florianpigis/src/App.js
+++ florianpigis/src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColorsComponent from './ColorsComponent'
+import data from './colors.json';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      color: "#000000",
+      colors: data.colors
+    }
+  }
+
+  changeColor(colorId){
+    const colors = this.state.colors;
+        
+    let newState = this.state;
+    colors.forEach(function(color){
+      if (color.id == colorId){
+        newState.color = color.color;
+      }
+    });
+
+    this.setState(newState);
+  }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" style={{background: this.state.color}}>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ColorsComponent colors={this.state.colors} changeColor={this.changeColor.bind(this)}/>
       </div>
     );
   }
