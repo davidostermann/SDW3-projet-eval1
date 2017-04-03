eea190fa373fc5ce84351455f4329033252649a6
Florian Pigis (PC fixe) <florian.pigis@gmail.com>
2017-04-03 21:01:03 +0200

    Ajout de couleur

--- florianpigis/src/App.js
+++ florianpigis/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ColorsComponent from './ColorsComponent'
+import ColorsComponent from './ColorsComponent';
 import data from './colors.json';
 
 class App extends Component {
@@ -11,7 +11,11 @@ class App extends Component {
 
     this.state = {
       color: "#000000",
-      colors: data.colors
+      colors: data.colors,
+      form: {
+        name: null,
+        color: null,
+      }
     }
   }
 
@@ -28,6 +32,40 @@ class App extends Component {
     this.setState(newState);
   }
 
+  handleChange(e){
+    let newState = this.state;
+    switch (e.target.name){
+      case "name":
+        newState.form.name = e.target.value;
+        break;
+      case "color":
+        newState.form.color = e.target.value;
+        break;
+    }
+
+    this.setState(newState);
+  }
+
+  addColor(event){
+    let newState = this.state;
+
+    const newColor = {
+      "id": newState.colors.length,
+      "name": this.state.form.name,
+      "color": this.state.form.color,
+    }
+
+    newState.colors.push(newColor);
+
+    this.setState(newState);
+  }
+
+  input = {
+    "border-radius": "4px",
+    "border-style": "solid",
+    "margin": "10px 5px"
+  }
+
   render() {
     return (
       <div className="App">
@@ -37,6 +75,15 @@ class App extends Component {
         </div>
         Quand je clique sur un element de la liste ci-dessous, je change la couleur de fond du header
         <ColorsComponent colors={this.state.colors} changeColor={this.changeColor.bind(this)}/>
+
+        <div style={{"text-align": "center", "width": "80%", "margin": "auto", "background-color":"lightgrey", "padding": "20px", "border-radius":"4px"}}>
+          <h3>Ajouter une couleur</h3>
+          <label>Nom</label><br />
+          <input type="text" name="name" onChange={this.handleChange.bind(this)} style={this.input} value={this.state.form.name}/><br />
+          <label>Couleur</label><br />
+          <input type="text" name="color" onChange={this.handleChange.bind(this)} style={this.input} value={this.state.form.color}/><br />
+          <input type="submit" style={this.input} onClick={this.addColor.bind(this)}/>
+        </div>
       </div>
     );
   }
