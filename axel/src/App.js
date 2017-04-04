fc55000ecaecbb81b6cd17f0c3d37548f9d40cc6
Axel-Charpentier <axelcharpentier0@gmail.com>
2017-04-04 09:22:41 +0200

    Exo finish

--- axel/src/App.js
+++ axel/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
-class App extends Component {
+export default class App extends Component {
+
+  state = { color: '#222', colors: data.colors, newColorName: '', newColorHex: '' };
+
+  addColor() {
+    const id = this.state.colors.length;
+    const name = this.state.newColorName;
+    const hex = this.state.newColorHex;
+    this.setState({ colors: [ ...this.state.colors, { id, name, hex }], newColorName: '', newColorHex: '' });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app__header" style={{ background: this.state.color }}>
+          <img src={ logo } className="app__logo" alt="logo" />
+          <h2>Coloring input</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="app__intro">Quand je clique sur un élément de la liste ci-dessous, je change la couleur de fond du header</p>
+        { this.state.colors.map( color => (
+          <button className="button" style={{ background: color.color }} onClick={ () => this.setState({ color: color.color }) }>{ color.name }</button>
+        )) }
+        <h2>Add a new color</h2>
+        <input className="field" placeholder="Color name" value={ this.state.newColorName } onChange={ (e) => this.setState({ newColorName: e.target.value }) } />
+        <input className="field" placeholder="Color hex" value={ this.state.newColorHex } onChange={ (e) => this.setState({ newColorHex: e.target.value }) } />
+        <button className="button" onClick={ this.addColor.bind(this) }>Add</button>
       </div>
     );
   }
 }
-
-export default App;
